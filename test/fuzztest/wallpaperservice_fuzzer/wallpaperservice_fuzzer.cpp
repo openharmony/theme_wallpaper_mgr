7cc30195e998f0661bdefb7450a2410b0bf3b9c6
ma-shaoyin <mashaoyin1@huawei.com>
2022-07-25 18:27:33 +0800

    Signed-off-by: ma-shaoyin <mashaoyin1@huawei.com>
     Changes to be committed:

--- test/fuzztest/wallpaperservice_fuzzer/wallpaperservice_fuzzer.cpp
+++ test/fuzztest/wallpaperservice_fuzzer/wallpaperservice_fuzzer.cpp
@@ -61,7 +61,7 @@ bool FuzzWallpaperService(const uint8_t* rawData, size_t size)
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     if (size < OHOS::THRESHOLD) {
-        return -1;
+        return 0;
     }
 
     /* Run your code on data */
