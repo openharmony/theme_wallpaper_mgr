a8a1b47cf06dff696ae2f3bb7b28d99e05deb876
LVB8189 <lvbai1@huawei.com>
2022-09-27 14:55:38 +0800

    Signed-off-by: LVB8189 <lvbai1@huawei.com>
     Changes to be committed:

--- services/src/component_name.cpp
+++ services/src/component_name.cpp
@@ -40,17 +40,9 @@ namespace WallpaperMgrService {
         return mClass;
     }
 
-    bool ComponentName::equals(ComponentName& obj)
+    bool ComponentName::equals(ComponentName &obj)
     {
-        try {
-                // Note: no null checks, because mPackage and mClass can
-                // never be null.
-                return mPackage==obj.GetPackageName()
-                        && mClass==obj.GetClassName();
-        } catch (std::exception e) {
-            // DO NOTHING
-        }
-        return false;
+        return mPackage == obj.GetPackageName() && mClass == obj.GetClassName();
     }
-}
+    }
 }
