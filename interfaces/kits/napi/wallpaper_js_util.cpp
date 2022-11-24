0a607c8328c7303f7caf552912b0b29cef676533
weishaoxiong <weishaoxiong1@huawei.com>
2022-11-24 19:38:19 +0800

    Signed-off-by: weishaoxiong <weishaoxiong1@huawei.com> fix: modify jsRgbaColor object

--- interfaces/kits/napi/wallpaper_js_util.cpp
+++ interfaces/kits/napi/wallpaper_js_util.cpp
@@ -18,11 +18,6 @@
 #include "hilog_wrapper.h"
 
 namespace OHOS::WallpaperNAPI {
-const static int32_t ARRAY_LENGTH = 4;
-const static int32_t ZERO = 0;
-const static int32_t ONE = 1;
-const static int32_t TWO = 2;
-const static int32_t THREE = 3;
 constexpr const uint32_t COLOR_MASK{ 0xFF };
 
 std::string WallpaperJSUtil::Convert2String(napi_env env, napi_value jsString)
@@ -63,21 +58,24 @@ napi_value WallpaperJSUtil::Convert2JSRgbaArray(napi_env env, const std::vector<
     int index = 0;
     for (const auto it : color) {
         HILOG_DEBUG("Convert2JSRgbaArray for");
-        ColorManager::Color colors(it);
+        napi_value red = nullptr;
+        napi_value green = nullptr;
+        napi_value blue = nullptr;
+        napi_value alpha = nullptr;
+        napi_create_int32(env, color.red, &red);
+        napi_create_int32(env, color.green, &green);
+        napi_create_int32(env, color.blue, &blue);
+        napi_create_int32(env, color.alpha, &alpha);
+        napi_create_int32(env, static_cast<int32_t>(colors.r * COLOR_MASK), &red);
+        napi_create_int32(env, static_cast<int32_t>(colors.g * COLOR_MASK), &green);
+        napi_create_int32(env, static_cast<int32_t>(colors.b * COLOR_MASK), &blue);
+        napi_create_int32(env, static_cast<int32_t>(colors.a * COLOR_MASK), &alpha);
         napi_value element = nullptr;
-        napi_create_array_with_length(env, ARRAY_LENGTH, &element);
-        napi_value jsRgba = nullptr;
-        napi_create_int32(env, static_cast<int32_t>(colors.r * COLOR_MASK), &jsRgba);
-        napi_set_element(env, element, ZERO, jsRgba);
-        jsRgba = nullptr;
-        napi_create_int32(env, static_cast<int32_t>(colors.g * COLOR_MASK), &jsRgba);
-        napi_set_element(env, element, ONE, jsRgba);
-        jsRgba = nullptr;
-        napi_create_int32(env, static_cast<int32_t>(colors.b * COLOR_MASK), &jsRgba);
-        napi_set_element(env, element, TWO, jsRgba);
-        jsRgba = nullptr;
-        napi_create_int32(env, static_cast<int32_t>(colors.a * COLOR_MASK), &jsRgba);
-        napi_set_element(env, element, THREE, jsRgba);
+        napi_create_object(env, &element);
+        napi_set_named_property(env, element, "red", red);
+        napi_set_named_property(env, element, "green", green);
+        napi_set_named_property(env, element, "blue", blue);
+        napi_set_named_property(env, element, "alpha", alpha);
         napi_set_element(env, result, index++, element);
     }
     return result;
