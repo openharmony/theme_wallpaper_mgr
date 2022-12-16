1afe671c695be6e78fef07e5797ca90c637b4608
LVB8189 <lvbai1@huawei.com>
2022-12-16 09:43:47 +0800

    modify crash
    Signed-off-by: LVB8189 <lvbai1@huawei.com>

--- services/src/wallpaper_service.cpp
+++ services/src/wallpaper_service.cpp
@@ -435,16 +435,17 @@ bool WallpaperService::SaveColor(int wallpaperType)
         return false;
     }
     std::vector<uint64_t> colors;
-    std::lock_guard<std::mutex> autoLock(listenerMapMutex_);
     if (wallpaperType == WALLPAPER_SYSTEM && !CompareColor(systemWallpaperColor_, color)) {
         systemWallpaperColor_ = color.PackValue();
         colors.emplace_back(systemWallpaperColor_);
+        std::lock_guard<std::mutex> autoLock(listenerMapMutex_);
         for (const auto listener : colorChangeListenerMap_) {
             listener.second->OnColorsChange(colors, WALLPAPER_SYSTEM);
         }
     } else if (wallpaperType == WALLPAPER_LOCKSCREEN && !CompareColor(lockWallpaperColor_, color)) {
         lockWallpaperColor_ = color.PackValue();
         colors.emplace_back(lockWallpaperColor_);
+        std::lock_guard<std::mutex> autoLock(listenerMapMutex_);
         for (const auto listener : colorChangeListenerMap_) {
             listener.second->OnColorsChange(colors, WALLPAPER_LOCKSCREEN);
         }
@@ -869,7 +870,7 @@ bool WallpaperService::On(sptr<IWallpaperColorChangeListener> listener)
         return false;
     }
     std::lock_guard<std::mutex> autoLock(listenerMapMutex_);
-    colorChangeListenerMap_.insert(std::pair(IPCSkeleton::GetCallingTokenID(), listener));
+    colorChangeListenerMap_.insert_or_assign(IPCSkeleton::GetCallingTokenID(), listener);
     HILOG_DEBUG("WallpaperService::On out");
     return true;
 }
