8a1d5d025e63d8bbb2729a3efac9360b685beefc
LVB8189 <lvbai1@huawei.com>
2023-01-20 17:15:41 +0800

    modify
    Signed-off-by: LVB8189 <lvbai1@huawei.com>

--- frameworks/kits/extension/src/js_wallpaper_extension.cpp
+++ frameworks/kits/extension/src/js_wallpaper_extension.cpp
@@ -141,7 +141,7 @@ void JsWallpaperExtension::OnStart(const AAFwk::Want &want)
         HILOG_INFO("jsWallpaperExtension->CallObjectMethod");
         HandleScope handleScope(jsWallpaperExtension->jsRuntime_);
         NativeEngine *nativeEng = &(jsWallpaperExtension->jsRuntime_).GetNativeEngine();
-        sptr<WorkData> workData = new WorkData(nativeEng, wallpaperType);
+        WorkData *workData = new (std::nothrow) WorkData(nativeEng, wallpaperType);
         uv_after_work_cb afterCallback = [](uv_work_t *work, int32_t status) {
             WorkData *workData = reinterpret_cast<WorkData *>(work->data);
             napi_value type = OHOS::AppExecFwk::WrapInt32ToJS(reinterpret_cast<napi_env>(workData->nativeEng_),
@@ -149,6 +149,10 @@ void JsWallpaperExtension::OnStart(const AAFwk::Want &want)
             NativeValue *nativeType = reinterpret_cast<NativeValue *>(type);
             NativeValue *arg[] = { nativeType };
             jsWallpaperExtension->CallObjectMethod("onWallpaperChanged", arg, ARGC_ONE);
+            delete workData;
+            workData = nullptr;
+            delete work;
+            work = nullptr;
         };
         UvQueue::Call(reinterpret_cast<napi_env>(nativeEng), workData, afterCallback);
         return true;
