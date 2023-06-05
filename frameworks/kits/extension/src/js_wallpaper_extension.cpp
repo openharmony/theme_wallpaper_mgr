7724586723c44f0beb42fc80afcea12f2596dfa0
DaiHN <daihuina1@huawei.com>
2023-06-05 15:42:55 +0800

    Adaptation of modular compilation issue : https://gitee.com/openharmony/theme_wallpaper_mgr/issues/I7AZRX?from=project-issue
    Signed-off-by: DaiHN <daihuina1@huawei.com>
    Change-Id: I583202b0e09fbcd873db15ea2040202eb34db073

--- frameworks/kits/extension/src/js_wallpaper_extension.cpp
+++ frameworks/kits/extension/src/js_wallpaper_extension.cpp
@@ -90,7 +90,8 @@ void JsWallpaperExtension::Init(const std::shared_ptr<AbilityLocalRecord> &recor
     HandleScope handleScope(jsRuntime_);
     auto &engine = jsRuntime_.GetNativeEngine();
 
-    jsObj_ = jsRuntime_.LoadModule(moduleName, srcPath, abilityInfo_->hapPath);
+    jsObj_ = jsRuntime_.LoadModule(moduleName, srcPath, abilityInfo_->hapPath,
+        Extension::abilityInfo_->compileMode == CompileMode::ES_MODULE);
     if (jsObj_ == nullptr) {
         HILOG_ERROR("Failed to get jsObj_");
         return;
