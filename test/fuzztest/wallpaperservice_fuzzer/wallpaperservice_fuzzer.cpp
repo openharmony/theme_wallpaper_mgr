4bbca6a60576226d260198d692c4f49d278b845c
LVB8189 <lvbai1@huawei.com>
2023-08-08 10:01:35 +0800

    modify
    Signed-off-by: LVB8189 <lvbai1@huawei.com>

--- test/fuzztest/wallpaperservice_fuzzer/wallpaperservice_fuzzer.cpp
+++ test/fuzztest/wallpaperservice_fuzzer/wallpaperservice_fuzzer.cpp
@@ -38,10 +38,8 @@ uint32_t ConvertToUint32(const uint8_t *ptr)
     return bigVar;
 }
 
-bool FuzzWallpaperService(const uint8_t *rawData, size_t size)
+bool FuzzWallpaperService(const uint8_t *rawData, size_t size, uint32_t code)
 {
-    uint32_t code =
-        ConvertToUint32(rawData) % (static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::SEND_EVENT) + 1);
     MessageParcel data;
     data.WriteInterfaceToken(WALLPAPERSERVICES_INTERFACE_TOKEN);
     data.WriteBuffer(rawData, size);
@@ -51,6 +49,101 @@ bool FuzzWallpaperService(const uint8_t *rawData, size_t size)
     std::shared_ptr<WallpaperService> wallpaperService = std::make_shared<WallpaperService>();
     wallpaperService->OnRemoteRequest(code, data, reply, option);
     return true;
+}
+
+bool FuzzSetWallpaperServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::SET_WALLPAPER);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzGetPixelMapServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::GET_PIXELMAP);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzGetColorsServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::GET_COLORS);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzGetWallpaperIdServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::GET_WALLPAPER_ID);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzGetFileServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::GET_FILE);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzResetWallpaperServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::RESET_WALLPAPER);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzOnServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::ON);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzOffServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::OFF);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzIsChangePermittedServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::IS_CHANGE_PERMITTED);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzIsOperationAllowedServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::IS_OPERATION_ALLOWED);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzRegisterCallbackServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::REGISTER_CALLBACK);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzSetWallpaperV9ServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::SET_WALLPAPER_V9);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzGetPixelMapV9ServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::GET_PIXELMAP_V9);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzGetColorsV9ServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::GET_COLORS_V9);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzResetWallpaperV9ServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::RESET_WALLPAPER_V9);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzSetVideoServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::SET_VIDEO);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzSetCustomServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::SET_CUSTOM);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzSendEventServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::SEND_EVENT);
+    return FuzzWallpaperService(rawData, size, code);
+}
+
+bool FuzzBoundaryCodeServiceTest(const uint8_t *rawData, size_t size){
+    uint32_t code = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::SEND_EVENT) + 1;
+    return FuzzWallpaperService(rawData, size, code);
 }
 } // namespace OHOS
 
@@ -62,6 +155,24 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     }
 
     /* Run your code on data */
-    OHOS::FuzzWallpaperService(data, size);
+    OHOS::FuzzSetWallpaperServiceTest(data, size);
+    OHOS::FuzzGetPixelMapServiceTest(data, size);
+    OHOS::FuzzGetColorsServiceTest(data, size);
+    OHOS::FuzzGetWallpaperIdServiceTest(data, size);
+    OHOS::FuzzGetFileServiceTest(data, size);
+    OHOS::FuzzResetWallpaperServiceTest(data, size);
+    OHOS::FuzzOnServiceTest(data, size);
+    OHOS::FuzzOffServiceTest(data, size);
+    OHOS::FuzzIsChangePermittedServiceTest(data, size);
+    OHOS::FuzzIsOperationAllowedServiceTest(data, size);
+    OHOS::FuzzRegisterCallbackServiceTest(data, size);
+    OHOS::FuzzSetWallpaperV9ServiceTest(data, size);
+    OHOS::FuzzGetPixelMapV9ServiceTest(data, size);
+    OHOS::FuzzGetColorsV9ServiceTest(data, size);
+    OHOS::FuzzResetWallpaperV9ServiceTest(data, size);
+    OHOS::FuzzSetVideoServiceTest(data, size);
+    OHOS::FuzzSetCustomServiceTest(data, size);
+    OHOS::FuzzSendEventServiceTest(data, size);
+    OHOS::FuzzBoundaryCodeServiceTest(data, size);
     return 0;
 }
