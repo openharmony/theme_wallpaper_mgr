8538691d6e7164269206a40370f50acea6fb5890
LVB8189 <lvbai1@huawei.com>
2023-08-08 20:09:32 +0800

    modify
    Signed-off-by: LVB8189 <lvbai1@huawei.com>

---
+++ test/fuzztest/on_fuzzer/on_fuzzer.cpp
@@ -0,0 +1,56 @@
+/*
+* Copyright (c) 2023 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#include "wallpaperservice_fuzzer.h"
+
+#include <cstddef>
+#include <cstdint>
+
+#include "message_parcel.h"
+#include "wallpaper_service.h"
+
+using namespace OHOS::WallpaperMgrService;
+
+namespace OHOS {
+constexpr size_t THRESHOLD = 4;
+constexpr uint32_t CODE = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::ON);
+const std::u16string WALLPAPERSERVICES_INTERFACE_TOKEN = u"ohos.Wallpaper.IWallpaperService";
+
+bool FuzzOnServiceTest(const uint8_t *rawData, size_t size)
+{
+    MessageParcel data;
+    data.WriteInterfaceToken(WALLPAPERSERVICES_INTERFACE_TOKEN);
+    data.WriteBuffer(rawData, size);
+    data.RewindRead(0);
+    MessageParcel reply;
+    MessageOption option;
+    std::shared_ptr<WallpaperService> wallpaperService = std::make_shared<WallpaperService>();
+    wallpaperService->OnRemoteRequest(CODE, data, reply, option);
+    return true;
+}
+
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    if (size < OHOS::THRESHOLD) {
+        return 0;
+    }
+
+    /* Run your code on data */
+    OHOS::FuzzOnServiceTest(data, size);
+    return 0;
+}
