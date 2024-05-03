f12db3ca81c0e80b733f7c4111fbd8deef9d498a
dengjun <dengjun39@huawei.com>
2024-05-03 10:27:04 +0800

    fix: 新增FUZZ用例
    Signed-off-by: dengjun <dengjun39@huawei.com>

--- test/fuzztest/wallpaperservice_fuzzer/src/setwallpaperpixelmapv9_fuzz.cpp
+++ test/fuzztest/wallpaperservice_fuzzer/src/setwallpaperpixelmapv9_fuzz.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-#include "sendevent_fuzzer.h"
+#include "setwallpaperpixelmapv9_fuzz.h"
 
 #include <cstddef>
 #include <cstdint>
@@ -24,7 +24,7 @@
 
 using namespace OHOS::WallpaperMgrService;
 constexpr size_t THRESHOLD = 4;
-constexpr uint32_t CODE = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::SEND_EVENT);
+constexpr uint32_t CODE = static_cast<uint32_t>(WallpaperServiceIpcInterfaceCode::SET_WALLPAPER_PIXELMAP_V9);
 
 /* Fuzzer entry point */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
