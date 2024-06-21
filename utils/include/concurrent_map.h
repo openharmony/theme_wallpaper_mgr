1209dad6e2688bb7e8e6fb121093a8953c97a96e
lvchao <lvchao28@huawei.com>
2024-06-21 10:25:01 +0800

    feat:整改
    Signed-off-by: lvchao <lvchao28@huawei.com>

--- utils/include/concurrent_map.h
+++ utils/include/concurrent_map.h
@@ -156,12 +156,6 @@ public:
         return count;
     }
 
-    mapped_type &operator[](const key_type &key) noexcept
-    {
-        std::lock_guard<decltype(mutex_)> lock(mutex_);
-        return entries_[key];
-    }
-
     void ForEach(const std::function<bool(const key_type &, mapped_type &)> &action)
     {
         if (action == nullptr) {
